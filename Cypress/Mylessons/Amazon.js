e423a2966b6222ddfa0820925f18d71632328f38
gulgulyil <72104981+gulgulyil@users.noreply.github.com>
2025-09-17 10:31:17 +0200

    test search button og category box

--- Cypress/Mylessons/Amazon.js
+++ Cypress/Mylessons/Amazon.js
@@ -11,4 +11,30 @@ describe("Amazon Testleri", () => {
     });
 
 
+    it("Url ve Title kontrolü", () => {
+        cy.url().should('include', 'amazon');
+        cy.title().should('include', 'Amazon');
+    });
+
+    it("Ürün arama ve 5. ürünü seçme", () => {
+        // Arama yap
+        cy.get('#twotabsearchtextbox').type('Samsung s23 Ultra {enter}');
+
+        // Arama sonuçlarının yüklenmesini bekle
+        cy.get('.s-result-list .s-result-item', { timeout: 10000 })
+            .should('have.length.greaterThan', 0);
+
+        // 5. ürünün görünür olmasını bekleyip tıkla
+        cy.get('.s-result-list .s-result-item')
+            .eq(4)
+            .scrollIntoView()
+            .should('be.visible')
+            .click();
+    });
+
+    it("Kategori seçim kutusunda Elektronik seç", () => {
+        cy.get('#searchDropdownBox').select('Elektronik');
+    });
+
+
 });
