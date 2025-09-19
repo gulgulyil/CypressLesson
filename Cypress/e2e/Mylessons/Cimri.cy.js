cf52ef88f24bf6022587eb237938602e1e52ddc2
gulgulyil <72104981+gulgulyil@users.noreply.github.com>
2025-09-19 15:24:21 +0200

    testet research button

--- Cypress/e2e/Mylessons/Cimri.cy.js
+++ Cypress/e2e/Mylessons/Cimri.cy.js
@@ -19,5 +19,13 @@ Describe("Cimri site testi", () => {
         cy.title().should('include', 'Cimri');
     });
 
+    it('Arama yapilmalidir ve sonuclar dogrulanmalidir', () => {
 
+        const expectedText = 'Asus rog zephyrus'
+
+        cy.get('.s17vz787-1').click();
+        cy.get('.s17vz787-4').type(expectedText)
+        cy.wait(2000)
+        cy.contains('asus rog zephyrus g14').click();
+    })
 })
