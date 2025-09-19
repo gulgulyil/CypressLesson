982f955c2a384e4796c1b293844de2b49ef40be1
gulgulyil <72104981+gulgulyil@users.noreply.github.com>
2025-09-19 15:32:00 +0200

    edited

--- cypress.config.js
+++ cypress.config.js
@@ -5,8 +5,8 @@ module.exports = defineConfig({
   e2e: {
     specPattern: "cypress/e2e/**/*.cy.js",
     baseUrl: "https://www.amazon.com.tr",
-    supportFile: "cypress/support/e2e.js"
-    viewportWidth: 1280;
-    viewportHeight: 720;
+    supportFile: "cypress/support/e2e.js",
+    viewportWidth: 1280,
+    viewportHeight: 720,
   }
 });
