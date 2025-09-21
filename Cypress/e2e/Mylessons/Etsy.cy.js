940c6c8bef4d30f182395bf775aac78e97fe8e48
gulgulyil <72104981+gulgulyil@users.noreply.github.com>
2025-09-21 17:43:08 +0200

    edited

--- Cypress/e2e/Mylessons/Etsy.cy.js
+++ Cypress/e2e/Mylessons/Etsy.cy.js
@@ -2,8 +2,14 @@ describe("Ürün Ara, Fiyata Göre Sirala ve Sepete Ekle", function () {
 
     beforeEach(() => {
         //Etsy web sitesine gidilir
-        cy.visit("https://etsy.com")
-        cy.contains('Accepter').click()
+        cy.visit("https://etsy.com", {
+            failOnStatusCode: false,
+            headers: {
+                'User-Agent':
+                    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0 Safari/537.36'
+            }
+        })
+        cy.contains('Accept').click()
         cy.url().should("i", "https:/etsy.com")
         cy.scrollTo("buttom");
     });
