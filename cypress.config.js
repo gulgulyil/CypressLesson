c49ba8e8f5eac68aea6bb75486c5573bff91b353
gulgulyil <72104981+gulgulyil@users.noreply.github.com>
2025-09-22 15:07:26 +0200

    edited config for cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,16 @@
 
-const { defineConfig } = require('cypress')
+const { defineConfig } = require('cypress');
+const createBundler = require('@bahmutov/cypress-esbuild-preprocessor');
+const { addCucumberPreprocessorPlugin } = require('@badeball/cypress-cucumber-preprocessor');
+const createEsbuildPlugin = require('@badeball/cypress-cucumber-preprocessor/esbuild');
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/**/*.cy.js",
+    // Feature dosyalarını da görecek şekilde pattern’i güncelledik
+    specPattern: [
+      "cypress/e2e/**/*.feature",   // Cucumber senaryoları
+      "cypress/e2e/**/*.cy.js"      // Mevcut normal testleriniz
+    ],
     supportFile: "cypress/support/e2e.js",
     viewportWidth: 1280,
     viewportHeight: 720,
@@ -14,9 +21,23 @@ module.exports = defineConfig({
       runMode: 2,
       openMode: 1
     },
-    // baseUrl'i environment değişkenine göre ayarla
+    // baseUrl environment’a göre
     baseUrl: process.env.TEST_ENV === 'etsy'
       ? 'https://www.etsy.com'
-      : 'https://www.amazon.com.tr'
+      : 'https://www.amazon.com.tr',
+
+    async setupNodeEvents(on, config) {
+      // <-- Cucumber için gerekli plugin kurulumu
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        'file:preprocessor',
+        createBundler({
+          plugins: [createEsbuildPlugin.default(config)],
+        })
+      );
+
+      return config;
+    }
   }
-})
+});
